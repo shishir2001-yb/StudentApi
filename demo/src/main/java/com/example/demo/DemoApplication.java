eeb936333f6dab7c9e020257fa25e249a6d86ace
shishir2001 <ansh01072001@gmail.com>
2022-02-01 02:21:22 +0530

    Initial Push

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> hello()
+	{
+		return List.of(
+				new Student(
+						1L,
+						"Shishir",
+						"shds2001@gmail.com",
+						LocalDate.of(2001, Month.JULY,1),
+						21
+
+				)
+		);
+	}
+}
